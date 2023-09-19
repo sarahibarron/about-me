9deaacc7bc19497641984e4f119c0b54982da744
SARAH BARRON <sarahibarron@hotmail.co.uk>
2023-09-19 15:13:34 +0100

    more java added

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-let answer1 = confirm("Are you ready?");
+console.log("Hello there");
 
-if (answer1 === true) {
-  console.log("You are ready!");
-} else {
-  console.log("Why are you not ready?");
+function greet() {
+  let username = prompt("What is your name?");
+  document.write(username + "!");
 }
 
-console.log(answer1);
+console.log("Hello " + username);
+
+function raccoonQ() {
+  let raccoonP = prompt("Do I like raccoons? (yes or no)");
+  let raccoonQ = raccoonP.toLowerCase();
+  if (raccoonQ === "yes") {
+    alert("Hell yes I do!");
+  } else if (raccoonQ === "no") {
+    alert("That's a lie! I love raccoons!");
+  }
+}
+
+function pizzaQ() {
+  let pizzaP = prompt("Will I eat meat on a pizza? (yes or no)");
+  let pizzaQ = pizzaP.toLowerCase();
+  if (pizzaQ === "yes") {
+    alert("No, how rude!");
+  } else if (pizzaQ === "no") {
+    alert("That's right, veggies only for meeee");
+  }
+}
